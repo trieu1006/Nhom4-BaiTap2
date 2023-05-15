e848336dc8c4d95b8a9af4a234e39a15fff82ded
Trieu <huynhquangtrieu10062001@gmail.com>
2023-05-15 15:02:44 +0700

    Thuc hanh bai tap 2 nhom 4

---
+++ thucHanhBaiTap2/Debug/Src/main.d
@@ -0,0 +1,29 @@
+Src/main.o: ../Src/main.c \
+ D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/temhumsensor.h \
+ D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h \
+ D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h \
+ D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h \
+ D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_i2c.h \
+ D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h \
+ D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h \
+ D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h \
+ D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_spi.h \
+ D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h \
+ D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h
+D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor/temhumsensor.h:
+D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h:
+D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h:
+D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h:
+D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_i2c.h:
+D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h:
+D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h:
+D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h:
+D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h:
+D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h:
+D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_spi.h:
+D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h:
+D:/Nam_4/HK2/HTNhung_IOT/SDK-NUCLEO/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h:
